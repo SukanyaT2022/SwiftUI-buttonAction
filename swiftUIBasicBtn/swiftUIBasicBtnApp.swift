5c6138924eefd743b3ea7b5b36b05572a4279084
yoon <sukanyat2022a@gmail.com>
2025-02-25 10:15:31 -0500

    Initial Commit

---
+++ swiftUIBasicBtn/swiftUIBasicBtnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBasicBtnApp.swift
+//  swiftUIBasicBtn
+//
+//  Created by TS2 on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIBasicBtnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUIBasicBtnTests/swiftUIBasicBtnTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBasicBtnTests.swift
+//  swiftUIBasicBtnTests
+//
+//  Created by TS2 on 2/25/25.
+//
+
+import Testing
+@testable import swiftUIBasicBtn
+
+struct swiftUIBasicBtnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
