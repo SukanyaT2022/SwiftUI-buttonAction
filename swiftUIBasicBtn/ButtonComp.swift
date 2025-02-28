677aa3f75eab6a7bc314f73a4356d1f134ef0115
yoon <sukanyat2022a@gmail.com>
2025-02-27 21:01:05 -0500

    first commit

---
+++ swiftUIBasicBtn/ButtonComp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonComp.swift
+//  swiftUIBasicBtn
+//
+//  Created by TS2 on 2/27/25.
+//
+
+import SwiftUI
+
+struct ButtonComp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonComp()
+}
---
+++ swiftUIBasicBtn/loginUIView.swift
@@ -0,0 +1,18 @@
+//
+//  loginUIView.swift
+//  swiftUIBasicBtn
+//
+//  Created by TS2 on 2/26/25.
+//
+
+import SwiftUI
+
+struct loginUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginUIView()
+}
